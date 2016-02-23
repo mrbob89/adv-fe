0d8c7d5915c6440ed13c06de2edf805cf6036ddc
bobo89 <johndoe@example.com>
2016-02-24 00:47:48 +0200

    Connect handlebars and show info on the page from data.js

---
+++ client_src/js/post.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+	var posts = Data.getRelatedPosts();
+	var comments = Data.getPostComments();
+
+	var templateRelPosts = Handlebars.compile( $("#related-posts-template").html() );
+	var templateComments = Handlebars.compile( $("#all-comments-template").html() );
+
+	Handlebars.registerPartial("relPost", $("#related-post-partial").html());
+	Handlebars.registerPartial("oneComment", $("#one-comment-partial").html());
+
+	var dataPosts = {
+		posts: function () {
+			return posts;
+		}
+    };
+    var dataComments = {
+    	comments: function () {
+			return comments;
+		}
+    }
+
+    $('.related-posts .posts').html(templateRelPosts(dataPosts));
+    $('.comments .row').html(templateComments(dataComments));
+
+});
