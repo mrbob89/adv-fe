4cd1537641e61aaf3dbffaa5b9b09b5f071b722f
bobo89 <johndoe@example.com>
2016-02-26 19:00:22 +0200

    Made updates

--- client_src/js/post.js
+++ client_src/js/post.js
@@ -3,22 +3,20 @@ $(document).ready(function() {
 	var posts = Data.getRelatedPosts();
 	var comments = Data.getPostComments();
 
-	var templateRelPosts = Handlebars.compile( $("#related-posts-template").html() );
-	var templateComments = Handlebars.compile( $("#all-comments-template").html() );
+	var templateRelatedPosts = Handlebars.compile( $("#related-posts-template").html() );
+	var templateComments = Handlebars.compile( $("#comments-template").html() );
 
-	Handlebars.registerPartial("relPost", $("#related-post-partial").html());
-	Handlebars.registerPartial("oneComment", $("#one-comment-partial").html());
+	Handlebars.registerPartial("related-post", $("#related-post-partial").html());
+	Handlebars.registerPartial("comment", $("#comment-partial").html());
 
 	var dataPosts = {
 		posts: posts,
-    };
-    var dataComments = {
-    	comments: function () {
-			return comments;
-		}
-    }
+	};
+	var dataComments = {
+		comments: comments,
+	}
 
-    $('.related-posts .posts').html(templateRelPosts(dataPosts));
-    $('.comments .row').html(templateComments(dataComments));
+	$('.related-posts .posts').html(templateRelatedPosts(dataPosts));
+	$('.comments .row').html(templateComments(dataComments));
 
 });
--- client_src/js/test.js
+++ client_src/js/test.js
@@ -1,34 +1,30 @@
 $(document).ready(function() {
 
-	var posts = Data.getPosts();
+    var posts = Data.getPosts();
 
-	var templateJsonPosts = Handlebars.compile( $("#posts-json-template").html() );
+    var templateJsonPosts = Handlebars.compile( $("#posts-json-template").html() );
     var templateTable = Handlebars.compile($("#table-json-template").html());
 
     Handlebars.registerPartial("description", $("#description-post-partial").html());
 
-	Handlebars.registerHelper( "json", function (allPosts){
-		return JSON.stringify(allPosts, null, ' ');
-	});
-
-    Handlebars.registerHelper("stripes", function(array, even, odd, options) {
-		var buffer = "";
-		for (var i = 0, j = array.length; i < j; i++) {
-			var item = array[i];
-
-			item.stripeClass = (i % 2 == 0 ? even : odd);
-
-			buffer += options.fn(item);
-		}
-
-		return buffer;
+    Handlebars.registerHelper( "json", function (obj){
+        return '<pre>' + JSON.stringify(obj," ", 4) + '</pre>';
     });
 
-	var dataPosts = {
-		posts: posts
+    Handlebars.registerHelper("stripes", function(array, options) {
+        var count = 0;
+        var buffer = array.map(function(item) {
+            item.stripeClass = (count % 2 == 0 ? "even" : "odd");
+            count++;
+            return options.fn(item); 
+        });
+        return buffer.join('\n');
+    });
+
+    var dataPosts = {
+        posts: posts
     };
 
     $('.post-json').html(templateJsonPosts(dataPosts));
     $('.posts-table').html(templateTable(dataPosts));
-
 });
